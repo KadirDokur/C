63400688d530df2e959ad44e536996bc6049e4b7
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-07-22 14:55:57 +0300

    modified:   IntroductionToC/placeholder.c

--- IntroductionToC/placeholder.c
+++ IntroductionToC/placeholder.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello");
-    printf("hello");
-    printf("hello");
+
+    int original[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int size = sizeof(original) / sizeof(original[0]);
+    int reversed[size];  // Array to store reversed elements
+
+    int k = 0;
+
+    printf("BEFORE REVERSING\n");
+    for (int i = 0; i < size; i++){
+        printf("%d ", original[i]);
+    }
+
+
+    for (int i = size - 1; i >= 0; i--){
+        reversed[k] = original[i];
+        k++;
+    }
+
+    printf("\nAFTER REVERSING\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",reversed[i]);
+    }
+    
 
     return 0;
 }
